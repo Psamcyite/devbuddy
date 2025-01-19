bec2a5f4a75527ce750edeee48ddd3ac09b9de1b
Psamcyite <amojoayokunle@gmail.com>
2025-01-19 11:36:25 +0100

    chore: integrate Vercel Analytics for app insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
         <main className="flex-grow">
           {children}
         </main>
+        <Analytics />
         <Footer />
       </body>
     </html>
